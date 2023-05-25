05d9f2c549e934e642c306c301604d3f05b43900
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-05-25 19:51:59 +0200

    new & this keywork

--- advanced-variables.js
+++ advanced-variables.js
@@ -155,14 +155,14 @@
 // console.log(book);
 
 //* -------------------------REFERENCE VS VALUE--------------------------*//
-//each array/object created has a NEW memory address, if the array/object is set as value of object then will share THE SAME memory address
+//each array/object created has a NEW memory address this is their "value", if the array/object is set as value of variable then will share THE SAME memory address
 //array and objects reference the same code unless they are not related, aka: mentioned as value from another variable
 
 // const a = 1;
 // const b = "hi";
 // const c = [2, 3];
-// const d = c; //this code is making C and D array refer to the same value so if one channge the other will too
-// d.push(4); // this code will push "4" to both variables B and C, because when it comes to array/onjects they are referencing the same "memory address" 0x01
+// const d = c; //this code is making C and D array refer to the same value or memory address so if one change the other will too
+// d.push(4); // this code will push "4" to both variables B and C, because when it comes to array/objects they are referencing the same "memory address" 0x01 if one is declared as value of another
 // console.log(a);
 // console.log(b);
 // console.log(c);//[2, 3, 4]
@@ -170,15 +170,15 @@
 
 // let a = [1, 2]; //one memory address
 // let b = [1, 2]; //another memory address
-// console.log(a === b); //return false, because variable a and b was nnever related so each has it's own memory address
+// console.log(a === b); //return false, because variable a and b was never related so each has it's own memory address
 // b.push(3);
 // console.log(a); // [1, 2]
 // console.log(b); //[1, 2, 3]
 
 // let person = { name: "leticia", age: 26 }; //one memory address
-// person = { name: "leticia", age: 26, sex: "Female" };
+// person = { name: "leticia", age: 26, sex: "Female" }; // pushing new key and value
 // let person2 = { name: "leticia", age: 26 }; //another memory address even tho same value inside
-// person.hobby = "Swimming"; //pushing only to one variable
+// person.hobby = "Swimming"; //pushing only to first variable
 // console.log(person); // { name: "leticia", age: 26, sex: "Female" , hobby: "Swimming"}
 
 // const a = [1, 2];
@@ -230,8 +230,9 @@
 // });
 // console.log(isTrue); // true
 
+//---------REDUCE METHOD
 //!!!!exercise: get the total of the code below using .reduce method
-//.reduce takes at least 2 paramethers .reduce(1, 2)
+//.reduce takes at least 2 paramethers .reduce(function, starting value that will be used in the reducer function) and reduce the array down to ONE SINGLE METHOD
 // const items = [
 //   { price: 10 },
 //   { price: 20 },
@@ -241,10 +242,76 @@
 // ];
 // const total = items.reduce((sum, item) => {
 //   return sum + item.price;
-// }, 0);
+// }, 0/*starting value as 0*/);
 // console.log(total);
 
 //includes: return true or false is array include that value
-const myName = ["Leticia", "Carolina", "Nascimento", "Azevedo"];
-const isTrue = myName.includes("Carolina");
-console.log(isTrue);
+// const myName = ["Leticia", "Carolina", "Nascimento", "Azevedo"];
+// const isTrue = myName.includes("Carolina");
+// console.log(isTrue); //true
+
+//* -------------------------STRING TEMPLATE LITERALS--------------------------*//
+/*ways to declare a string on a variable ""  ''  ``*/
+
+// const a = "Hi";
+// const b = "Hello";
+// const c = `Hey`;
+// console.log(a + " " + b + " " + c); //Hi Hello Hey
+// //easier way to declare same console.log above with backticks
+// console.log(`${a} ${b} ${c}`); //Hi Hello Hey , syntax `${any js code or variable to run here}`
+
+//exercise: create 2 variables with your name and combina them together with backticks
+// const first = "Leticia";
+// const last = "Azevedo";
+// console.log(`${first} ${last}`);// Leticia Azevedo // this backticks syntax is called template literals or template string
+
+//* -------------------------NEW & THIS keyword--------------------------*//
+
+//when you are creating an object that will be repeated, use first letter as capital for best pratice like the Date object
+// const today = new Date();//this "Date" is essentially an object that is converted to a string before logged out //new keyword is a constructor
+// console.log(today);
+
+// function person(name, age) {
+//   return { name: name, age: age, human: true };
+// }
+// function NewPerson(name, age) {
+//   // this = {} calling "this" it creates a new empty object and we will push the key&values to it
+//   this.name = name; //'this' keyword reference the current object you are trying to create
+//   this.age = age;
+//   this.human = true;
+//   //(return this) at the end it will return the object "this"
+// } //this is a constructor function, because it will construct a new code/object that will be repeated
+// const values = person("Luca", 31);
+// const newValues = new NewPerson("Leticia", 26);
+// console.log(values); //{name: 'Luca', age: 31, human: true}
+// console.log(newValues); //NewPerson {name: 'Leticia', age: 26, human: true}
+
+function car(model, brand, year) {
+  return {
+    model: model,
+    brand: brand,
+    year: year,
+  };
+}
+
+// function NewCar(
+//   model,
+//   brand,
+//   year /*parameters will defined based on which function it will construct*/
+// ) {
+//   (this.model = model),
+//     (this.name = " "),
+//     (this.brand = brand),
+//     (this.year = year); //() curly brackets here are optional
+// }
+
+class NewCar {
+  constructor(model, brand, year) {
+    (this.model = model), (this.brand = brand), (this.year = year);
+  }
+}
+
+const newValues = new NewCar("Renegade", "Jeep", 2014); // the keyword "new" creates a new variable using the same paramets as the variable "car"
+const values = car("Model S", "Tesla", 2022);
+console.log(values);
+console.log(newValues);
