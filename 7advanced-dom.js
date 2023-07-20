e6a21297d59c09ccfe5c94cc0b9961afa0626b59
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-07-20 15:01:12 -0300

    browser storage

--- 7advanced-dom.js
+++ 7advanced-dom.js
@@ -74,7 +74,9 @@
 // });
 
 //* --------------------------------62. EVENT LOOP------------------------------*//
-//the way javascript run the code
+//event loop is the way javascript run the code
+//usually promises have priority over others async code, they can run after the main thread is over or after the function it's in is over.
+//the key is that Promises/async funnctions will run eventually when they have time to and generally run quicker than things like timeouts because of the separate queue they are in.
 
 //javascript run code in a line called "MAIN THREAD"
 //any async code will run on a different thread, and queue up to run after the main one is finished
@@ -93,26 +95,103 @@
 
 // functions behave differently than others async like settime and addevent
 //promises and async will go to the queue immediately after the function it's in finishes reading.
-function sayHi() {
-  console.log("hi 1");
-  setTimeout(() => {
-    console.log("hi 2");
-  }, 30);
-  new Promise((resolve) => {
-    resolve("hi 3");
-  }).then((a) => {
-    console.log(a);
-  });
-  setTimeout(() => {
-    console.log("hi 4");
-  }, 0);
-  console.log("hi 5");
-}
-sayHi();
+// function sayHi() {
+//   console.log("hi 1");
+//   setTimeout(() => {
+//     console.log("hi 2");
+//   }, 30);
+//   new Promise((resolve) => {
+//     resolve("hi 3");
+//   }).then((a) => {
+//     console.log(a);
+//   });
+//   setTimeout(() => {
+//     console.log("hi 4");
+//   }, 0);
+//   console.log("hi 5");
+// }
+// sayHi();
 //hi 1, hi 5, hi 3, hi 4, hi 2
 //hi 1, hi 5 (MAIN THREAD)
 //hi 3 logged out after function started to run
 //hi 4, hi 2 queued up accordingly to the settimeout specifically
-for (i = 0; i < 1000; i++) {
-  console.log("First things first");
-}
+
+//* --------------------------------63. EVENT DELEGATION------------------------------*//
+//the layers of javascript
+//for example when you click a button, it's behind the scenes also clicking the body and the document
+//when you click on a button or any event on the page, these events will be captured and bubbled, starting with capturing from the outside/document to the inside/button in this case, and then bubbling up from the inside/button to the furthest/outside (document)
+//code is run on the bubble phase
+
+// const buttons = document.querySelectorAll("button");
+// document.addEventListener("click", () => {
+//   console.log("clicked document");
+// });
+
+// document.body.addEventListener("click", () => {
+//   console.log("clicked body");
+// });
+
+// buttons.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     console.log("clicked button");
+//   });
+// }); //sequence of output: clicked button, clicked body, clicked document
+
+// const buttons = document.querySelectorAll("button");
+// document.addEventListener("click", () => {
+//   console.log("clicked document");
+// });
+
+// document.body.addEventListener(
+//   "click",
+//   () => {
+//     console.log("clicked body");
+//   },
+//   { capture: true } // setting capture to true will capture this event first before the button
+// );
+
+// buttons.forEach((e) => {
+//   e.addEventListener("click", (e) => {
+//     e.stopPropagation(); //this will stop the browser from propagating and won't capture body and document.
+//     console.log("clicked button");
+//   });
+// }); //sequence of output: clicked button, clicked body (baucause body has a  { capture: true } otherwise wouldn't be logged)
+
+//creating a new button and also adding the event listener to this new button
+// const button = document.querySelector("button");
+// const newButton = document.createElement("button");
+// newButton.innerHTML = " Button 2";
+// document.body.append(newButton);
+// // //will NOT log clicked on both, code is only being added to button and not newButton
+// // button.addEventListener("click", () => {
+// //   console.log("clicked");
+// // });
+// //RESOLUTION
+// //this code has no connection with variable declared on previous example, it targets the document and ask for the matching of "button"
+// document.addEventListener("click", (e) => {
+//   if (e.target.matches("button")) {
+//     console.log("clicked");
+//   } //.matches(name as targeret on css)
+// });
+
+//* ----------------------------64. BROWSER STORAGE--------------------------*//
+//3 different main types of browser storage: cookies, local storage and session storage
+// Local Storage       |  Cookies              | Session Storage
+// 10 MB (a lot)       | 4KB                   | 5MB                   //you're gonna get limit of for each item inside
+// data never expires  | set manual expiration | Expire on tab close   //when the data expires?
+// only on user browser| client and server     | only on user browser //how can you access this information? *cookies are perfect for when u need to send data from the client/user to the server automatically
+// Easy to use         | Hard                  | Easy to use
+
+//can access with keyword localStorage,since it is an object has methods on it
+//syntax .setItem("key - you choose", "value - always string") syntax to save a new item on the storage
+localStorage.setItem("Name", "Leticia");
+//updated the same key "name", it will not add a new key
+localStorage.setItem("Name", "Carolina");
+//deleting an item .removeItem('key')
+// localStorage.removeItem("Name"); //case-sensitive
+//same for session Storage
+sessionStorage.setItem("Name", "Leticia");
+sessionStorage.setItem("Name", "Azevedo");
+sessionStorage.removeItem("Name");
+//getting data from the storage with .getItem("key")
+console.log(localStorage.getItem("Name"));
