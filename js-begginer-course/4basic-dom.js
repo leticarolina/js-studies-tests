5b73753023d9908a86ed56a65825c8975c82b0dd
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-12-19 14:03:03 -0800

    review basic dom

--- js-begginer-course/4basic-dom.js
+++ js-begginer-course/4basic-dom.js
@@ -15,7 +15,7 @@ the browser will also understand without*/
 /*document is the keyword we use to access the html document, also add elements to html or change them*/
 
 // console.log(document); //prints out the whole html page including <!DOCTYPE html>
-// console.log(document.body); //prints only
+// console.log(document.body); //prints only body html
 // console.log(document.documentElement); //prints out only the body and head portion oh html
 
 //creating a new element on the html using document in 3 steps
@@ -25,63 +25,103 @@ the browser will also understand without*/
 
 /*-------------------------- 37. ID AND CLASS SELECTORS-----------------------*/
 
-//calling an id element
+//tagging and calling an id element
 // const id = document.getElementById("div-id"); //inside curly brackets goes the name of the class or ID
 // console.log(id);
 
 //changing css style of an ID
 // const divWithId = document.getElementById("div-id");
 // console.log(divWithId);
-// divWithId.style.color = "red"; // the text inside div-id will turn into red this code won't work for class because class is an array
+// divWithId.style.color = "red"; // the text inside div-id will turn into red this code won't work for class because class in this code is an array
 
-//selecting elements by their class and changing color
-// const divWithClass = document.getElementsByClassName("div-class"); // this code return htmlCollection which is an object similar to an array
+// const classR = document.querySelectorAll("div-class");
+// console.log(classR); // NodeList []
+// classR.style.color = "red"; //this wont work
+
+//right way to select elements by their class and changing color
+// const divWithClass = document.getElementsByClassName("div-class"); // this code return htmlCollection which is an object similar to an array but without the array methods
 // console.log(divWithClass); //HTMLCollection(3) [div.div-class, div.div-class, div.div-class]
-// divWithClass[0].style.color = "green"; //this will change color of only the first div, this is kinda of an array but without the array methods
+// divWithClass[0].style.color = "green"; //this will change color of only the first div
 // converting HTMLcollection divs into an actual array and then modyfing the color of it all
-// const classToArray = Array.from(divWithClass); // Array.from(variable) method syntax to transform something into an array, in this case the div-class
+// const classToArray = Array.from(divWithClass); // Array.from(variable) method syntax to transform something into an array, in this case the div-class variable
 // classToArray.forEach((element) => {
 //   element.style.color = "green";
 // }); //this code is telling to take classToArray variable and for each array run the function
 
 /*-------------------------- 38. QUERY SELECTOR-----------------------*/
 /*select any element regardless it's type, querySelector will select first element that match and return the html element, querySelectorAll will select all elements that match and turn into a Nodelist (sort of an array) with only forEach method included*/
-/*querySelectorAll has forEach method without needing to convert into an array before*/
+/*querySelectorAll return nodeList and has forEach method without needing to convert into an array before*/
 
 //selecting a data element
-// const dataElement = document.querySelector("[data-test]");
+// const dataElement = document.querySelector("[data-test]"); //with querySelecter the data element has to be inside []
 // dataElement.style.color = "orange";
 // console.log(dataElement);
 
 //selecting divs by class with querySelectorAll
 // const divWithClass = document.querySelectorAll(".div-class"); //need to put class as it would be in CSS
-// divWithClass.forEach((div) => (div.style.color = "green")); //creating a loop for each array and then changing it's color, no need to change to array with query bcs it has forEach method appended
+// divWithClass.forEach((div) => (div.style.color = "green")); //this function is kinda creating a loop running for each div and then changing it's color, no need to change to array before bcs with query it has forEach method appended
 
 //selecting an input
 // const input = document.querySelector("input");
-// console.log(input); // <input>
-// //can be more specific
-// const inputSpecific = document.querySelector("input[type='text']");
-// console.log(inputSpecific); //<input type="text">
+// console.log(input); // <input data-input type="text">
+
+//can be more specific selecting an element
+// const inputSpecific = document.querySelector("input[data-input]");
+// const inputSpecific2 = document.querySelector("input[type='checkbox']");
+// console.log(inputSpecific); // <input data-input type="text" />
+// console.log(inputSpecific2); //<input data-input-second type="checkbox" />
 
 /*-------------------------- 39. EVENT LISTENERS-----------------------*/
+//https://blog.webdevsimplified.com/2022-01/event-listeners/
 
-//event listener on a button from html
-//create a variable to target the button
+//adding an event listener on a button from html
+//first create a variable to target the button
 // const myButton = document.querySelector("[data-button]");
 // //addinng an event listener to the button variable
 // myButton.addEventListener("click", (afterClick) => {
+//   afterClick.preventDefault(); // since button is inside a form
 //   console.log("Clicked");
 // }); //syntax nameOfVariable.addEventListener(2 parameters) first is name of the event seconf is the function that will run after event
 
+//Running Events Once
+//need to declare a third parameter to addEventListener that has a property called once, if once set to true will ensure eventListener only runs one time.
+// const myButton = document.querySelector("[data-once]");
+// myButton.addEventListener(
+//   "click",
+//   () => {
+//     console.log("clicked");
+//   },
+//   { once: true }
+// );
+
+// removing an event listener
 // const myButton = document.querySelector("[data-button]");
 // myButton.addEventListener("click", afterClick);
-// //removing an event listener
 // myButton.removeEventListener("click", afterClick);
 // //function created to be the second parameter of both the event listeners
 // function afterClick() {
 //   console.log("Clicked");
 // } //nothing will happen on the console after clicking bcs the event is being added and remove right after on the same click
+
+//Abort anEvent Listeners
+//creating an event listener that works until a certain condition is met
+// let count = 0;
+// const controller = new AbortController(); //creating a new AbortController
+// const myButton = document.querySelector("[data-once]");
+// myButton.addEventListener(
+//   "click",
+//   () => {
+//     count = count + 1;
+//     console.log(count);
+
+//     if (count >= 5) {
+//       controller.abort(); //calling the abort method on our AbortController
+//     } //event listener will stop running after condition
+//   },
+//   { signal: controller.signal }
+//   //passing the signal property of it to the signal property of the options object for our addEventListener
+//   //This connects our event listener to that AbortController so if we abort it will remove the event listener.
+// );
 
 //addidng event listener to an input
 // const input = document.querySelector("[data-input]");
@@ -89,9 +129,9 @@ the browser will also understand without*/
 // function inputChanged() {
 //   console.log("The input value has been changed");
 // } // "change" event listener will run once the input has been changed and mouse clicked out of the input
-//using "input" event instead of "change" will return how many characters are being written on the input
+//using "input" event instead of "change" will run according to how many characters are being written on the input
 
-//Using 'submit' event listener on the submit button from a form
+//Using 'submit' event listener on the form, any button clicked inside the form will submit form
 // const submitButton = document.querySelector("[data-form]");
 // submitButton.addEventListener("submit", (clicked) => {
 //   clicked.preventDefault(); //this code is to prevent the default behaviour when running code**
@@ -101,20 +141,23 @@ the browser will also understand without*/
 
 //PREVENTING THE DEFAULT BEHAVIOUR
 //exercise: Create an anchor <a> and when its clicked prevent the default (opening the link) and log something to the console instead
-// const site = document.querySelector("[data-anchor]");
-// site.addEventListener("click", (a) => {
-//   a.preventDefault(); //this method prevents the link to be opened and will run code below instead
-//   console.log("The link was clicked");
+// const anchor = document.createElement("a");
+// anchor.innerText = "link";
+// anchor.href = "https://www.canva.com/projects";
+// document.body.appendChild(anchor);
+// anchor.addEventListener("click", (clicked) => {
+//   clicked.preventDefault();
+//   console.log("prevented");
 // });
 //same result of exercise on a different way
-// const theLink = document.querySelector("a");
+// const theLink = document.querySelector("#google-link");
 // theLink.addEventListener("click", recalculate);
 // function recalculate(aa) {
 //   aa.preventDefault();
 //   console.log("The link was clicked");
 // }
 
-//TYPES OF Event
+//TYPES OF Event listener
 /*mouseenter = everytime mouse hover will trigger the event listener
 mouseleave = whenever the mouse leave hovering the object will trigger the event listener
 mouseover = mouse over will only run once when the mouse is over an element (?)
@@ -124,60 +167,71 @@ rezise = whenever the user resixe the window browser */
 
 /*----------------NORMAL VS ARROW FUNCTION (using 'this' keyword)---------------*/
 
-//always use arrow function is you will use the keyword 'this'
+//always use arrow function if you will use the keyword 'this'
 // const button = document.querySelector("[data-button]");
-// //adding evennt listener with arrow function to log 'this'
+
+//adding evennt listener with arrow function to log 'this'
 // button.addEventListener("click", (theEvent) => {
+//   theEvent.preventDefault();
 //   console.log(this);
 // }); //'this' will access a window object (means that can do things like this.document inside the arrow function only)
-// //adding evennt listener with normal function to log 'this'
+
+//adding evennt listener with normal function to log 'this'
 // button.addEventListener("click", function theEvent(a) {
-//   console.log(this);
-// }); //this will access the html code for button, if I would write console.log(a.targe) would return the same
+//   a.preventDefault();
+//   console.log(this); //  <button data-button type="submit">Button 1</button>
+//   console.log(a.target); //  <button data-button type="submit">Button 1</button>
+// }); //this will access the html code for button
 
 /*----------------------------DATA ATTRIBUTES------------------------*/
 
-// const dataOne = document.querySelector("[data-one]");
-// //dataset will give a javascript object with all data values it has inside
-// // console.log(dataOne.dataset); // return {one: '123'} "one" is from data-one="123" name I gave. (If you dont give a value for data it will return empty string)
-// // console.log(dataOne.dataset.one); //return only value of the data "123"
-// //changing value of data attribute inside javascript (instead of HTML)
-// dataOne.dataset.one = "456"; //this will change 123 to 456 only on javascript not html (same way as changing value of an object)
+//dataset method will return a javascript object with all data values it has inside
+// const dataOne = document.querySelector("[data-content]");
+// console.log(dataOne.dataset); // {content: '123'} "content" is from name I gave data-content="123". (If you dont give a value for data it will return empty string)
+// console.log(dataOne.dataset.content); //123 //return only value of the data
 
-//this code will chage inside the html data-clicks= "" the amount of times the button is clicked
-// const buttons = document.querySelectorAll("button");
+//changing value of data attribute inside javascript (instead of using HTML)
+// dataOne.dataset.content = "456"; //this will change 123 to 456 only on javascript not html (same way as changing value of an object)
+// console.log(dataOne.dataset.content); //456
+
+//this code will change inside the html the amount of times the button is clicked data-clicks=""
+// const buttons = document.querySelectorAll("[data-clicks]");
 // buttons.forEach((theButton) => {
 //   theButton.addEventListener("click", () => {
 //     const dataToNumber = parseInt(theButton.dataset.clicks); //.clicks is from the data-clicks on html
 //     theButton.dataset.clicks = dataToNumber + 1;
+//     console.log(dataToNumber);
 //   });
 // });
 
 /*----------------------------DOM TRAVERSAL------------------------*/
 /*traverse = atravessar*/
 //key elements = children, nextElementSibling, previousElementSibling, parentElement
-/*  <div id="grand-parent">
-      Grand Parent 
-      <div>
-        Parent 1
-          <div id="child-one" class='child' >Child 1</div>
-          <div class='child'>Child 2</div>
-      </div>
-      <div> Parent 2</div>
-    </div> */
 
-//changing the color of only the first children of Parent 1
+{
+  /* <div id="grand-parent">
+  Grand Parent
+  <div>
+    Parent 1
+    <div id="child-one" class="child">
+      Child 1
+    </div>
+    <div class="child">Child 2</div>
+  </div>
+  <div> Parent 2</div>
+</div>; */
+}
 
 //getting the parent element of a child
 // const childOne = document.querySelector("#child-one");
 // const itsParent = childOne.parentElement; //getting the parent element of the target
-// itsParent.style.color = "red";
+// itsParent.style.color = "red"; //will change parent and child color
 // //getting the parent of itsParent
 // const grandParent = itsParent.parentElement;
-// grandParent.style.color = "blue";
+// grandParent.style.color = "blue"; //will change parent and child color
 
 //jumping from the child straight to the grand parent, without having to pass thru elements in between using method closest()
-//*important because not always you will know how many children/parent there is
+//*important code because not always you will know how many children/parent there is
 // const childOne = document.querySelector("#child-one");
 // const grandParent = childOne.closest("#grand-parent"); //closest method look for all of the parents and finds the FIRST/closest one that matches with selector we put in () that can be class, id etc
 // grandParent.style.color = "red";
@@ -187,7 +241,7 @@ rezise = whenever the user resixe the window browser */
 // const childrens = grandParent.querySelectorAll(".child"); //queryselectorAll will select all elements that match with class child
 // childrens.forEach((c) => {
 //   c.style.color = "red";
-// });//forEach because it is more than one element then need to create a function
+// }); //forEach because it is more than one element then need to create a function
 
 /*----------------------------TO DO LIST CODE------------------------*/
 // //1. Select all elements
@@ -207,9 +261,9 @@ rezise = whenever the user resixe the window browser */
 //   list.appendChild(item); //syntax parentHTMLElement.appendChild(variable)
 //   //2.3 clear the input after clicking to add the value
 //   input.value = "";
-//   //2.4 Setup event listener to delete item when click on it 2 ways
+//   //2.4 Setup event listener to delete item when click on it (2 ways)
 //   item.addEventListener("click", (deleted) => {
 //     list.removeChild(item); //this will remove the child from the list
-//     item.remove(); //method remove() willremove this individual item
+//     item.remove(); //method remove() will remove this individual item
 //   });
 // });
